5c2a816bdf5d20ad02bf998140c7117767dd5d95
Houngtze <xiaofeilovem@vip.qq.com>
2018-04-11 18:39:54 +0800

    base-bulid

--- src/main.js
+++ src/main.js
@@ -1,7 +1,50 @@
 import Vue from 'vue'
-import App from './App.vue'
+import App from './App'
+import router from './router'
+import axios from 'axios'
 
+import MuseUI from 'muse-ui'
+import 'muse-ui/dist/muse-ui.css'
+
+import VueAwesomeSwiper from 'vue-awesome-swiper'
+
+Vue.use(MuseUI)
+Vue.use(VueAwesomeSwiper)
+
+Vue.config.productionTip = false
+
+Vue.filter('time', function(value) {
+    value=value*1000
+    let date = new Date(value)
+    let m = date.getMonth() + 1
+    let d = date.getDate()
+    let h = date.getHours()
+    let i = date.getMinutes()
+    let s = date.getSeconds()
+    if (m < 10) {
+        m = '0' + m
+    }
+    if (d < 10) {
+        d = '0' + d
+    }
+    if (h < 10) {
+        h = '0' + h
+    }
+    if (i < 10) {
+        i = '0' + i
+    }
+    if (s < 10) {
+        s = '0' + s
+    }
+    var t =m+'-'+d+' '+h+':'+i+':'+s
+    return t
+})
+
+
+/* eslint-disable no-new */
 new Vue({
   el: '#app',
-  render: h => h(App)
+  router,
+  template: '<App/>',
+  components: { App }
 })
